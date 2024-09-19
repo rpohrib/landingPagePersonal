d14cf7a414c87aaba57fc417af72817c2ca530aa
«roberto» <rpohrib@iesebre.com>
2024-09-19 18:48:08 +0200

    modificar textos

--- main.js
+++ main.js
@@ -11,12 +11,13 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hola, aquesta es la meva primera pàgina web</h1>
+    <h3>Tonto quien lo lea</h3>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      Click on the Vite logo to go to 
     </p>
   </div>
 `
